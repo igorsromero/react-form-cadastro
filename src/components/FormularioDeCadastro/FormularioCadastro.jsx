d400f8e97c5bc63e96b0bca0a64eaa4082fe01b1
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-27 10:14:26 -0300

    Formulários Controlados // useState

--- src/components/FormularioDeCadastro/FormularioCadastro.jsx
+++ src/components/FormularioDeCadastro/FormularioCadastro.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+    const [nome, setNome] = useState("");
     return (
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" fullWidth margin="normal" />
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            console.log(nome)
+        }}>
+            <TextField
+                value={nome}
+                onChange={event => {
+                    setNome(event.target.value);
+                    if (nome.length >= 3) {
+                        setNome(nome.substr(0, 3));
+                    }
+                }}
+                id="nome" label="Nome" variant="outlined" fullWidth margin="normal" />
             <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
             <TextField id="CPF" label="CPF" variant="outlined" fullWidth margin="normal" />
             <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary" />} />
